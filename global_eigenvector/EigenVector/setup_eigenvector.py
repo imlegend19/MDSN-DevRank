ceaf853158915bee910bbc52f68101842a4e2e6d
Mahen Gandhi <mahen.gandhi@civilmachines.com>
2019-07-21 16:12:23 +0530

    added global eigenvector dict

---
+++ global_eigenvector/EigenVector/setup_eigenvector.py
@@ -0,0 +1,38 @@
+import pickle
+
+url = '/home/imlegend19/PycharmProjects/Research - Data Mining/global_eigenvector/EigenVector/' \
+      'definition_1/global_eigenvector_fc.txt'
+
+with open(url, 'rb') as fp:
+    ge = pickle.load(fp)
+
+# total : 1134
+
+ev_centrality = []
+who = [x for x in range(1134)]
+
+for i in range(1134):
+    ev_centrality.append(ge[i].real + ge[i + 1134].real
+                         + ge[i + (1134 * 2)].real + ge[i + (1134 * 3)].real)
+
+z = [x for (y, x) in sorted(zip(ev_centrality, who), key=lambda pair: pair[0], reverse=True)]
+# print(z)
+
+ev_centrality.sort(reverse=True)
+# print(ev_centrality)
+
+with open('/home/imlegend19/PycharmProjects/Research - Data Mining/adjacency_matrix/relative_id.txt', 'rb') as fp:
+    rel_id = pickle.load(fp)
+
+relative_id = {v: k for k, v in rel_id.items()}
+
+final_ev_who = []
+for i in range(1134):
+    final_ev_who.append((relative_id[z[i]], ev_centrality[i]))
+
+with open('global_ev_dict.txt', 'wb') as fp:
+    pickle.dump(final_ev_who, fp)
+
+print(final_ev_who)
+
+print("Process Finished!")
