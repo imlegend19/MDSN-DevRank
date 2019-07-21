87d382c8ceddce2fd2eabd2a6fa71362e8772eac
Mahen Gandhi <mahen.gandhi@civilmachines.com>
2019-07-21 15:30:28 +0530

    added avg fixed time of each developer

--- assignee_bug/script.py
+++ assignee_bug/script.py
@@ -1,7 +1,9 @@
-from urllib import request
 import pickle
+from urllib import request
+
 from local_settings import db
 from bs4 import BeautifulSoup
+from dateutil import parser
 
 with db:
     print("Connected to db...")
@@ -19,11 +21,15 @@ with db:
     for i in cur.fetchall():
         who_id_who[i[1].strip()] = i[0]
 
+    no_comment_assignee = []
+
     print("Filtering assignee_bug...")
     segregated_assignees = {}
     for i in assignees:
         if i in who_id_who.keys():
             segregated_assignees[i] = who_id_who[i]
+        else:
+            no_comment_assignee.append(i)
 
     print(segregated_assignees)
     print(len(segregated_assignees))
@@ -32,9 +38,15 @@ with db:
 
     assignee_bug = {}
     for i in cur.fetchall():
-        assignee_bug[i[1].strip()] = i[0]
+        if i[1].strip() in assignee_bug:
+            lst = assignee_bug[i[1].strip()]
+            lst.append(i[0])
+            assignee_bug[i[1].strip()] = lst
+        else:
+            assignee_bug[i[1].strip()] = [i[0]]
 
     print(len(assignee_bug))
+    print(assignee_bug)
 
     for i in list(assignee_bug):
         if i not in segregated_assignees.keys():
@@ -44,31 +56,71 @@ with db:
 
     url = "https://bugzilla.gnome.org/show_activity.cgi?id="
 
-    # cnt = 1
-    # for i in assignee_bug.values():
-    #    print("Ongoing", cnt)
-    #    request.urlretrieve(url + str(i), "bug_html/" + str(i) + ".html")
-    #    cnt += 1
+    cnt = 0
 
     for i in assignee_bug.values():
-        with open("bug_html/" + str(i) + ".html", 'r') as fp:
-            html = fp.read()
+        cnt += len(i)
 
-        soup = BeautifulSoup(html, features="html.parser")
+    assignee_fixed_time = {}
+    assignee_pet_name = []
 
-        div = soup.find("div", attrs={"id": "bugzilla-body"})
-        table = div.find("table")
+    for i in assignee_bug.values():
+        for j in i:
+            # print("Remaining", cnt)
+            # request.urlretrieve(url + str(j), "bug_html/" + str(j) + ".html")
+            # cnt -= 1
 
-        headings = [th.get_text() for th in table.find("tr").find_all("th")]
+            print("Ongoing", j)
 
-        print(headings)
+            with open('bug_html/' + str(j) + '.html', 'rb') as fp:
+                html = fp.read()
 
-        datasets = []
-        for row in table.find_all("tr")[1:]:
-            dataset = list(td.get_text().replace("\n", "").strip() for td in row.find_all("td"))
-            datasets.append(dataset)
+            soup = BeautifulSoup(html, features="html.parser")
 
-        print(datasets)
+            div = soup.find("div", attrs={"id": "bugzilla-body"})
+            table = div.find("table")
 
-        with open("bug_table/" + str(i) + ".txt", 'wb') as file:
-            pickle.dump(datasets, file)
+            headings = [th.get_text() for th in table.find("tr").find_all("th")]
+
+            # print(headings)
+
+            data = []
+            for row in table.find_all("tr")[1:]:
+                dataset = list(td.get_text().replace("\n", "").strip() for td in row.find_all("td"))
+                data.append(dataset)
+
+            # print(data)
+
+            assignee_pet_name.append(data[0][0])
+
+            # with open('bug_table' + str(j) + '.txt', 'wb') as fp:
+            #     pickle.dump(data, fp)
+
+            assigned_time = parser.parse(data[0][1])
+
+            it = -1
+            while True:
+                if data[it][-1] == 'VERIFIED' or data[it][-1] == 'WONTFIX':
+                    while True:
+                        if len(data[it]) == 3:
+                            it -= 1
+                        else:
+                            break
+                    break
+                else:
+                    it -= 1
+
+            finished_time = parser.parse(data[it][1])
+
+            assignee = list(assignee_bug.keys())[list(assignee_bug.values()).index(i)]
+
+            if assignee in assignee_fixed_time:
+                time = assignee_fixed_time[assignee][0] + (finished_time - assigned_time)
+                cnt = assignee_fixed_time[assignee][1] + 1
+
+                assignee_fixed_time[assignee] = [time, cnt]
+            else:
+                assignee_fixed_time[assignee] = [finished_time - assigned_time, 1]
+
+    with open('assignee_fixed_time.txt', 'wb') as fp:
+        pickle.dump(assignee_fixed_time, fp)
