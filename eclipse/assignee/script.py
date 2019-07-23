f7700bd40a3d6813240210f11255c5fcd9a5322b
Mahen Gandhi <mahen.gandhi@civilmachines.com>
2019-07-23 09:11:24 +0530

    added assignee script

---
+++ eclipse/assignee/script.py
@@ -0,0 +1,45 @@
+import pickle
+from urllib import request
+
+from local_settings_eclipse import db
+from bs4 import BeautifulSoup
+from dateutil import parser
+
+with db:
+    print("Connected to db...")
+
+    cur = db.cursor()
+
+    cur.execute("SELECT DISTINCTROW bug_id, assigned_to FROM test_bugs_fixed_closed")
+
+    assignee_bug = {}
+    for i in cur.fetchall():
+        if i[1] in assignee_bug:
+            lst = set(assignee_bug[i[1]])
+            lst.add(i[0])
+            assignee_bug[i[1]] = list(lst)
+        else:
+            assignee_bug[i[1]] = [i[0]]
+
+    print(len(assignee_bug))
+    print(assignee_bug)
+
+    with open("assignee_bug.txt", 'wb') as fp:
+        pickle.dump(assignee_bug, fp)
+
+    print(len(assignee_bug))
+
+    url = "https://bugs.eclipse.org/bugs/show_activity.cgi?id="
+
+    bugs = set()
+    for i in assignee_bug.values():
+        for j in i:
+            bugs.add(j)
+
+    cnt = len(bugs)
+    for i in bugs:
+        print("Remaining", cnt)
+        request.urlretrieve(url + str(j), "bug_html/" + str(j) + ".html")
+        cnt -= 1
+
+    print("Process Finished!")
