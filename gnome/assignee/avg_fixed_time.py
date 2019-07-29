c9c06f88a55818f9d42c298b27146052b9270942
Mahen Gandhi <mahen.gandhi@civilmachines.com>
2019-07-30 01:12:23 +0530

    modified correlation in both datasets

--- gnome/assignee/avg_fixed_time.py
+++ gnome/assignee/avg_fixed_time.py
@@ -2,7 +2,7 @@ import pickle
 
 import datetime
 
-with open('assignee_fixed_time.txt', 'rb') as fp:
+with open('assignee_first_fixed_time.txt', 'rb') as fp:
     assignee_fixed_time = pickle.load(fp)
 
 with open('assignee_who.txt', 'rb') as fp:
@@ -14,7 +14,7 @@ for i in assignee_fixed_time:
     j = assignee_fixed_time[i]
     assignee_avg_fixed_time[assignee_who[i]] = datetime.timedelta(seconds=((j[0].days * 86400 + j[0].seconds) / j[1]))
 
-with open('assignee_avg_fixed_time.txt', 'wb') as fp:
+with open('assignee_avg_first_fixed_time.txt', 'wb') as fp:
     pickle.dump(assignee_avg_fixed_time, fp)
 
 print(assignee_avg_fixed_time)
--- gnome/correlation/script.py
+++ gnome/correlation/script.py
@@ -17,7 +17,7 @@ sheet = wb.active
 
 titles = ["Assignee Id", "L1 Centrality", "L2-A Centrality", "L2-B Centrality", "L3 Centrality",
           "Global Centrality A", "Global Centrality B", "Avg Fixed Time", "Reopened Percent",
-          "Assignee Component", "Total Bugs", "Priority", "Severity"]
+          "Assignee Component", "Total Bugs", "Average First Closed Time", "Priority", "Severity"]
 
 sheet.append(titles)
 sheet.append(["" for i in range(len(titles))])
@@ -38,52 +38,86 @@ avg_fixed_time = fetch_file(RELATIVE_PATH + "assignee/assignee_avg_fixed_time.tx
 reopened_percent = fetch_file(RELATIVE_PATH + "assignee/assignee_reopened.txt")
 assignee_comp = fetch_file(RELATIVE_PATH + "assignee/assignee_component.txt")
 tot_bugs = fetch_file(RELATIVE_PATH + "assignee/assignee_total_bugs.txt")
+avg_first_time = fetch_file(RELATIVE_PATH + "assignee/assignee_avg_first_fixed_time.txt")
 priority = fetch_file(RELATIVE_PATH + "assignee/assignee_priority_count.txt")
 severity = fetch_file(RELATIVE_PATH + "assignee/assignee_severity_count.txt")
+
+
+def get_priority_points(priority):
+    """
+    Normal = 2
+    Low = 1
+    High = 3
+    Urgent = 4
+    Immediate = 5
+    """
+    points = 0
+    for i in priority:
+        if i == 'Low':
+            points += priority[1]
+        elif i == 'Normal':
+            points += priority[i] * 2
+        elif i == 'High':
+            points += priority[i] * 3
+        elif i == 'Urgent':
+            points += priority[i] * 4
+        else:
+            points += priority[i] * 5
+
+    return points
+
+
+def get_severity_points(severity):
+    """
+    normal = 3
+    critical = 5
+    major = 4
+    trivial = 1
+    minor = 2
+    blocker = 6
+    """
+    points = 0
+    for i in severity:
+        if i == 'normal':
+            points += severity[i] * 3
+        elif i == 'critical':
+            points += severity[i] * 5
+        elif i == 'major':
+            points += severity[i] * 4
+        elif i == 'trivial':
+            points += severity[i] * 1
+        elif i == 'minor':
+            points += severity[i] * 2
+        elif i == 'blocker':
+            points += severity[i] * 6
+
+    return points
+
 
 for i in who:
     w_a = who_assignee[i]
 
     try:
         l1 = l1_centrality[i]
-    except KeyError:
-        l1 = '-'
-
-    try:
         l2_d1 = l2_d1_centrality[i]
-    except KeyError:
-        l2_d1 = '-'
-
-    try:
         l2_d2 = l2_d2_centrality[i]
-    except KeyError:
-        l2_d2 = '-'
-
-    try:
         l3 = l3_centrality[i]
-    except KeyError:
-        l3 = '-'
-
-    try:
         gl_a = global_d1_centrality[i]
-    except KeyError:
-        gl_a = '-'
-
-    try:
         gl_b = global_d2_centrality[i]
-    except KeyError:
-        gl_b = '-'
 
-    avg = avg_fixed_time[i].days * 24 + avg_fixed_time[i].seconds / 3600
-    rp = reopened_percent[i]
-    comp = assignee_comp[i]
-    bugs = tot_bugs[i]
-    pri = str(priority[i])
-    sev = str(severity[i])
+        avg = avg_fixed_time[i].days * 24 + avg_fixed_time[i].seconds / 3600
+        rp = reopened_percent[i]
+        comp = assignee_comp[i]
+        bugs = tot_bugs[i]
+        avg_ft = avg_first_time[i].days * 24 + avg_first_time[i].seconds / 3600
+        pri = get_priority_points(priority[i])
+        sev = get_severity_points(severity[i])
 
-    row = [i, l1, l2_d1, l2_d2, l3, gl_a, gl_b, avg, rp, comp, bugs, pri, sev]
+        row = [i, l1, l2_d1, l2_d2, l3, gl_a, gl_b, avg, rp, comp, bugs, avg_ft, pri, sev]
 
-    sheet.append(row)
+        sheet.append(row)
+    except Exception:
+        pass
 
 wb.save("correlation_gnome_1.xlsx")
 print("Finished!")
