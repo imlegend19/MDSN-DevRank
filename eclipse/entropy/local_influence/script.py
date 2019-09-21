a0aa33eb87c0c8c30e4f5da5d8df86af8e13ac2c
imlegend19 <mahengandhi19@gmail.com>
2019-09-21 14:07:59 +0530

    added local influence files definition 1

---
+++ eclipse/entropy/local_influence/script.py
@@ -0,0 +1,48 @@
+import pickle
+import networkx as nx
+import math
+
+layer = 4
+
+PATH_EDGES = "/home/imlegend19/PycharmProjects/Research - Data Mining/eclipse/edges/definition_1/"
+
+with open(PATH_EDGES + "layer" + str(layer) + "_edges_fc.txt", 'rb') as fp:
+    edges = pickle.load(fp)
+
+PATH_NEIGHBOUR = "/home/imlegend19/PycharmProjects/Research - Data Mining/eclipse/neighbours/definition_1/"
+
+with open(PATH_NEIGHBOUR + "layer_" + str(layer) + "_neighbours.txt", 'rb') as fp:
+    neighbors = pickle.load(fp)
+
+graph = nx.DiGraph()
+graph.add_edges_from(edges)
+
+count = len(list(graph.nodes))
+loc = {}
+for i in graph.nodes:
+    print("Remaining:", count)
+    sub_graph = graph.subgraph(neighbors[i])
+
+    total_degree = 0
+    degrees = {}
+    for (node, val) in sub_graph.degree:
+        degrees[node] = val + 1
+        total_degree += val + 1
+
+    local_influence = 0
+    for j in sub_graph.nodes:
+        local_influence += (degrees[j] / total_degree) * math.log(degrees[j], 10)
+
+    local_influence += (len(neighbors[i]) / (total_degree + len(neighbors[i]))) * math.log(len(neighbors[i]), 10)
+
+    loc[i] = -local_influence
+
+    count -= 1
+
+print(loc)
+
+with open("definition_1/layer_" + str(layer) + "_local_influence.txt", 'wb') as fp:
+    pickle.dump(loc, fp)
+
+with open("definition_2/layer_" + str(layer) + "_local_influence.txt", 'wb') as fp:
+    pickle.dump(loc, fp)
