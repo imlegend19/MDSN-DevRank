b385bea510ae91de4ccff9d13839ff8e65d9d6e0
imlegend19 <mahengandhi19@gmail.com>
2019-09-24 23:26:41 +0530

    added gephi analysis

---
+++ netbeans/gephi_analysis/script.py
@@ -0,0 +1,35 @@
+import pickle
+import openpyxl
+
+wb_obj = openpyxl.load_workbook("layer1_analysis.xlsx")
+
+sheet_obj = wb_obj.active
+
+layer = 1
+
+degree = {}
+ec = {}
+close = {}
+harm_close = {}
+bet = {}
+for i in range(2, sheet_obj.max_row+1):
+    degree[sheet_obj.cell(i, 1).value] = sheet_obj.cell(i, 2)
+    close[sheet_obj.cell(i, 1).value] = sheet_obj.cell(i, 3)
+    harm_close[sheet_obj.cell(i, 1).value] = sheet_obj.cell(i, 4)
+    bet[sheet_obj.cell(i, 1).value] = sheet_obj.cell(i, 5)
+    ec[sheet_obj.cell(i, 1).value] = sheet_obj.cell(i, 6)
+
+with open("degree/degree_layer_" + str(layer) + ".txt", 'wb') as fp:
+    pickle.dump(degree, fp)
+
+with open("eigenvector/eigenvector_layer_" + str(layer) + ".txt", 'wb') as fp:
+    pickle.dump(ec, fp)
+
+with open("closeness/closeness_layer_" + str(layer) + ".txt", 'wb') as fp:
+    pickle.dump(close, fp)
+
+with open("betweenness/betweenness_layer_" + str(layer) + ".txt", 'wb') as fp:
+    pickle.dump(bet, fp)
+
+with open("harmonic_closeness/harmonic_closeness_layer_" + str(layer) + ".txt", 'wb') as fp:
+    pickle.dump(harm_close, fp)
